5da526b9f95fb34c0d1d0b92a631d5946afd70e8
detroitnatif <tytydizzle124@gmail.com>
2024-07-29 10:33:26 -0400

    adding weights from colab

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,18 +1,28 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n')
+model = YOLO('/Users/tylerklimas/Desktop/landslide/best.pt')
 
-# results = model.predict('/Users/tylerklimas/Desktop/landslide/input_video.mp4', save=True)
+results = model.predict('/Users/tylerklimas/Desktop/landslide/input_video.mp4', save=True)
 
-# print(results[0]) # prints first frame
+print(results[0]) # prints first frame
 
-# for box in results[0].boxes:
-#     print(box)
+for box in results[0].boxes:
+    print(box)
 
 # !pip install roboflow
 
-from roboflow import Roboflow
-rf = Roboflow(api_key="PTO9J5nMBYNNpj4SWYDi")
-project = rf.workspace("viren-dhanwani").project("tennis-ball-detection")
-version = project.version(6)
-dataset = version.download("yolov5")
+# from roboflow import Roboflow
+# rf = Roboflow(api_key="PTO9J5nMBYNNpj4SWYDi")
+# project = rf.workspace("viren-dhanwani").project("tennis-ball-detection")
+# version = project.version(6)
+# dataset = version.download("yolov5")
+
+# import shutil
+# shutil.move('tennis-ball-detection-6/train',
+#             'tennis-ball-detection-6/tennis-ball-detection-6/train')
+
+# shutil.move('tennis-ball-detection-6/test',
+#             'tennis-ball-detection-6/tennis-ball-detection-6/test')
+
+# shutil.move('tennis-ball-detection-6/valid',
+#             'tennis-ball-detection-6/tennis-ball-detection-6/valid')
